88d5e17ebffabcd9fe740ec304bfdb9b4adbf725
Vedant Mulherkar <72485535+VedantMulherkar1211@users.noreply.github.com>
2025-01-12 21:35:32 +0530

    Delete Start directory

--- Start/hello.js
+++
@@ -1,8 +0,0 @@
-const math = require("./math"); // Import the math module
-const req= require("")
-console.log("Hello World");
-
-console.log(math.add(2, 3)); // Correct usage of the add function
-console.log(math.sub(5,6));
-
-console.log(math.mult(3,3));
